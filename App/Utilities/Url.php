2d27c12c72ad7ea4cb66ca497dece5ca61cd4f27
mohammadmohebbi33 <mohammad.rasoul.mohebi@gmail.com>
2023-02-06 22:35:25 +0330

    front controller

---
+++ App/Utilities/Url.php
@@ -0,0 +1,14 @@
+<?php 
+
+class Url{
+
+    public static  function current()
+    {
+        return (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === "on" ? "https" : "http") . '://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; 
+    }
+
+
+    
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+ echo "front controller" ; 
+
+
+
+
+
+?>
